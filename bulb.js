0b5fc845887d1ac7fc5cda5f40e7f8ca2a52114c
Dave <58633928+ajokodavid@users.noreply.github.com>
2021-12-29 00:09:39 +0100

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,11 @@
+let onlight = document.getElementById("onlight");
+let offLight = document.getElementById("offlight");
+let image = document.querySelector("img");
+
+function onBulb() {
+    image.src = "images/bulbon.jpg";
+}
+
+function offBulb() {
+    image.src = "images/bulboff.jpg";
+}
